1b3504448e9af0a32295e7f783f021a4a838fb74
Jong <jm991014@gmail.com>
2025-11-23 03:35:32 +0900

    feat: Home UI 구현

--- app/src/main/java/com/sopt/sopkathon_android1/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/sopkathon_android1/presentation/home/HomeScreen.kt
@@ -1,24 +1,39 @@
 package com.sopt.sopkathon_android1.presentation.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.sopt.sopkathon_android1.R.drawable.image_logo
+import com.sopt.sopkathon_android1.core.designsystem.theme.SOPKATHONTheme
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-    Box(
+    Column(
         modifier = modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center
     ) {
+        Image(
+            painter = painterResource(image_logo),
+            contentDescription = null,
+        )
         Text(
             text = "Home"
         )
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    SOPKATHONTheme {
+        HomeScreen()
     }
 }
