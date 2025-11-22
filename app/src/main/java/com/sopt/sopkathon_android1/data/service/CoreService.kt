e0bd1bdadc2f90bf34ecb5530779b3c3b47012a2
김종우 <kimjw111003@gmail.com>
2025-11-23 03:32:03 +0900

    feat: header 추가 및 path 변경

--- app/src/main/java/com/sopt/sopkathon_android1/data/service/CoreService.kt
+++ app/src/main/java/com/sopt/sopkathon_android1/data/service/CoreService.kt
@@ -13,6 +13,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CoreService {
@@ -26,22 +27,22 @@ interface CoreService {
     fun getParticipatingBalanceGame() : Response<BaseResponse<ParticipatingBalanceGameResponse>>
 
     @GET("api/v1/balance-games/{balanceGameId}")
-    fun getBalanceGameInfo(@Query("balanceGameId") balanceGameId: Int) : Response<BaseResponse<BalanceGameInfo>>
+    fun getBalanceGameInfo(@Path("balanceGameId") balanceGameId: Int) : Response<BaseResponse<BalanceGameInfo>>
 
     @POST("api/v1/balance-games/{balanceGameId}/comments")
-    fun writeComment(@Query("balanceGameId") balanceGameId: Int, @Body writeCommentRequest: WriteCommentRequest) : Response<BaseResponse<Unit>>
+    fun writeComment(@Path("balanceGameId") balanceGameId: Int, @Body writeCommentRequest: WriteCommentRequest) : Response<BaseResponse<Unit>>
 
     @GET("api/v1/balance-games/{balanceGameId}/comments")
-    fun getComments(@Query("balanceGameId") balanceGameId: Int) : Response<BaseResponse<CommentResponse>>
+    fun getComments(@Path("balanceGameId") balanceGameId: Int) : Response<BaseResponse<CommentResponse>>
 
     @POST("api/v1/banace-games/{balanceGameId}/likes")
-    fun likeBalanceGame(@Query("balanceGameId") balanceGameId: Int, @Body likeRequest: LikeRequest) : Response<BaseResponse<Unit>>
+    fun likeBalanceGame(@Path("balanceGameId") balanceGameId: Int, @Body likeRequest: LikeRequest) : Response<BaseResponse<Unit>>
 
     @GET("api/v1/balance-games/past-participants")
     fun getParticipatedBalanceGame() : Response<BaseResponse<ParticipatedBalanceGameResponse>>
 
     @POST("api/v1/balance-games/{balanceGameId}")
-    fun voteBalanceGame(@Query("balanceGameId") balanceGameId: Int, @Body voteBalanceGameRequest: VoteBalanceGameRequest) : Response<BaseResponse<Unit>>
+    fun voteBalanceGame(@Path("balanceGameId") balanceGameId: Int, @Body voteBalanceGameRequest: VoteBalanceGameRequest) : Response<BaseResponse<Unit>>
 
     @GET("api/v1/balance-games?category")
     fun getAllBalanceGame(@Query("category") category: String) : Response<BaseResponse<BalanceGameAllResponse>>
