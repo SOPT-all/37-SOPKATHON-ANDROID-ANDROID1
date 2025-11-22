9d2667a655ebe4d20990525193d09f8d2944e5f0
너의이름 <qkrcksal1052@gmail.com>
2025-11-23 03:36:11 +0900

    [feat/#13] 공통 컴포넌트 topappbar 제작 불필요한 임포트 삭제

--- app/src/main/java/com/sopt/sopkathon_android1/core/designsystem/component/SopkathonTopappbar.kt
+++ app/src/main/java/com/sopt/sopkathon_android1/core/designsystem/component/SopkathonTopappbar.kt
@@ -2,12 +2,9 @@ package com.sopt.sopkathon_android1.core.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
